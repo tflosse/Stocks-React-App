20b9a272657dde01d03c7f27714361b3cc37c841
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-06-23 23:01:17 -0700

    Updates

---
+++ precache-manifest.c30b36cdb4dcd387e8b0e49b3b3dc692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23c67f81394c7a536ba9b60a31cb36e",
+    "url": "/stocks-API-app/stock-app/index.html"
+  },
+  {
+    "revision": "cb239aa8e93eb0941bd0",
+    "url": "/stocks-API-app/stock-app/static/css/main.afb2ed32.chunk.css"
+  },
+  {
+    "revision": "d502eacb4c33a61c794a",
+    "url": "/stocks-API-app/stock-app/static/js/2.f904f809.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stocks-API-app/stock-app/static/js/2.f904f809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb239aa8e93eb0941bd0",
+    "url": "/stocks-API-app/stock-app/static/js/main.05e8641c.chunk.js"
+  },
+  {
+    "revision": "9254b2d64ddbbafdaa15",
+    "url": "/stocks-API-app/stock-app/static/js/runtime-main.10e60476.js"
+  }
+]);
